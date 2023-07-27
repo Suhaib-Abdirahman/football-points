58ef9034b7d146610da65a3cd20002afb600ea5e
Suhaib Abdirahman <139591958+Suhaib-Abdirahman@users.noreply.github.com>
2023-07-27 13:52:44 +0300

    Worked

--- challenge.js
+++ challenge.js
@@ -1,2 +1,9 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+let defaultWin = 3,
+    defaultDraw = 1,
+    defaultLoss = 0;
+
+function footballPoints(wins, draws, losses) {
+    return defaultWin * wins + defaultDraw * draws + defaultLoss * losses;
+}
+
+console.log(footballPoints(3, 4, 2)); // 13
